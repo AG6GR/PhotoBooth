f51ec3054a0641c334b001efbfc544eaff71193f
AG6GR <slhe@princeton.edu>
2018-04-21 18:19:11 -0400

    Add basic html pages

--- photobooth.py
+++ photobooth.py
@@ -9,28 +9,49 @@ from threading import Thread
 
 # Physical pin 13
 GPIO_PIN = 27
+
+STATES = {
+    'WELCOME' : '/welcome.html',
+    'PROCESSING' : '/processing.html',
+    'RESULT' : '/result.html'
+}
+current_state = STATES['WELCOME']
+
+# Camera setup
 camera = picamera.PiCamera()
+camera.resolution = (1200, 720)
 latest_image = ''
+
+def get_state():
+    return current_state
 
 # HTTP Server request handler
 class MyRequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         print("GET request!", self.path)
-        if self.path == '/latest.jpg':
+        if self.path == '/latest.jpeg':
             print("redirect to:", '/' + latest_image)
             self.path = '/' + latest_image
+        elif self.path == '/kiosk':
+            print("State is", get_state())
+            if get_state() == STATES['RESULT']:
+                print("result display")
+                self.path = '/' + latest_image
+            else:
+                self.path = get_state()
         return SimpleHTTPRequestHandler.do_GET(self)
 
 
 #GPIO Callback
 def onButton(channel):
-        global latest_image
+        global latest_image, current_state
         print("Callback Triggered")
         if channel == GPIO_PIN:
-                print("Taking photo")
-                latest_image = 'image{:0}.jpg'.format(int(time.time()))
-                camera.capture(latest_image)
-                print(latest_image)
+            print("Taking photo")
+            latest_image = 'image{:0}.jpg'.format(int(time.time()))
+            camera.capture(latest_image)
+            print(latest_image)
+        current_state = STATES["PROCESSING"]
         # Reload webpage
         subprocess.run(["xdotool", "search", "--desktop", "0", "--class", "chromium", "windowactivate", "--sync", "key", "ctrl+F5"])
 
@@ -45,7 +66,7 @@ def main():
 
     httpd = HTTPServer(('', 8000), MyRequestHandler)
 
-    chrome_proc = subprocess.Popen(["chromium-browser", "http://localhost:8000/latest.jpg"])
+    chrome_proc = subprocess.Popen(["chromium-browser", "http://localhost:8000/kiosk"])
     httpd.serve_forever()
 
 if __name__ == "__main__":
